25ec1cd1504755cc5ea806874cb5a4af0c65aed7
Jimeno0 <carlospj@icloud.com>
2016-10-27 16:35:49 +0200

    iteration 3 & refactored code fot shopping cart

--- shopping_cart.rb
+++ shopping_cart.rb
@@ -3,13 +3,8 @@ require 'pry'
 class ShoppingCart
   def initialize
     @items = {}
-    @products = {
-      apples: 10,
-      oranges: 5,
-      grapes: 15,
-      banana: 20,
-      watermelon: 50
-    }
+    @products = Prices.get_season_prices
+    binding.pry
   end
 
   def add_item(sym)
@@ -55,17 +50,40 @@ class ShoppingCart
 
   def add_free_banana(grapes)
     if grapes >= 4
-      puts "You can get #{grapes/4} free bananas because you bought #{grapes} grapes"
+      puts "You can get #{grapes/4} bananas free (1 each 4 grapes)"
+    end
+  end
+end
+
+class Prices
+  def self.get_season_prices
+    binding.pry
+    time = Time.new
+    month = time.month
+    if month == 12 || month <=2
+      @products = {apples: 12,oranges: 5,grapes: 15,banana: 21,watermelon: 50}
+    elsif month >2 && month <=5
+      @products = {apples: 10,oranges: 5,grapes: 15,banana: 20,watermelon: 50}
+    elsif month >5 && month <=8
+      @products = {apples: 10,oranges: 2,grapes: 15,banana: 20,watermelon: 50}
+    elsif month >8 && month <=11
+      @products = {apples: 15,oranges: 5,grapes: 15,banana: 20,watermelon: 50}
     end
   end
 end
 
 
+#Iterarion 2
 # Buy 2 apples and pay just one!
 # Buy 3 oranges and pay just 2!
 # Buy 4 grapes you get one banana for free if you want!
 
-
+#Iterarion 3
+#            Spring Summer Autumn Winter
+# apples        10$    10$    15$    12$
+# oranges        5$     2$     5$     5$
+# grapes        15$    15$    15$    15$
+# banana        20$    20$    20$    21$
 
 cart = ShoppingCart.new
 
@@ -82,4 +100,5 @@ cart.add_item :grapes
 cart.add_item :grapes
 cart.show
 
+binding.pry
 puts cart.cost
